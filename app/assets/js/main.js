e943ab47943c6d55a78bce4b16cd1e9f4e39bdd9
dimitrije <kovacevic.d14@gmail.com>
2018-10-31 01:23:43 +0100

    vol.2

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,11 +1,6 @@
 //Mobile nav
 $(document).ready(function() {
-	$(".item-menu").on("click", function(){
-		$("nav ul").toggleClass("item-showing");
-	});
-	$(window).scroll(function(){
-		var scroll = $(window).scrollTop();
-		if (scroll > 100){
-			$("nav ul").removeClass("item-showing");
-		}
-	});
+	$(".item-menu").click(function(){
+			$("ul").slideToggle(500);
+	})
+});
